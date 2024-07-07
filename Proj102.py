1bfafae613c0fc3c1622c8d8e868315230882d6a
ArjunSharma216 <149858468+ArjunSharma216@users.noreply.github.com>
2024-07-07 10:57:46 +1000

    Add files via upload

---
+++ Proj102.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+
+from_dir="C:/Users/arjun_7oho2i/Downloads"
+to_dir="C:/Users/arjun_7oho2i/Downloads"
+list_of_files=os.listdir(from_dir)
+
+for file_name in list_of_files:
+    name, extension = os.path.splitext(file_name)
+    if extension == '':
+        continue
+    if extension in ['.txt', '.pdf','.doc', '.docx']:
+        path1 = from_dir + '/' + file_name
+        path2 = to_dir + '/' + "Documents"
+        path3 = to_dir + '/' + "Documents" + '/' + file_name
+
+        if os.path.exists (path2):
+            print ("Moving" + file_name + "...")
+            shutil.move (path1, path3)
+        else:
+            os.makedirs (path2)
+            print ("Moving" + file_name + "...")
+            shutil.move (path1, path3)
